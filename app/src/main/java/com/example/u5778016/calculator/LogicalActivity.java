9f83a7e5a63cfec9b1ce7204c1fa14c5381b757b
Siqi  Zhang <u5721067@anu.edu.au>
2016-05-14 20:02:26 +1000

    logical activity

---
+++ app/src/main/java/com/example/u5778016/calculator/LogicalActivity.java
@@ -0,0 +1,14 @@
+package com.example.u5778016.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LogicalActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logical);
+    }
+
+}
